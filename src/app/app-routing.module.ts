ab525c9fe1675a531c7d2fd042c4d9edaaef39d0
CORTEX-0241\raul.dionisio_cortex <raul.dionisio@cortex-intelligence.com>
2023-10-13 16:07:31 -0300

    Anotações e estudos sobre rotas com Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TitleComponent } from './pages/index/title/title.component';
+import { CardComponent } from './pages/portifolio/card/card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", // rota vazia - cai na home do site
+    component: TitleComponent,
+    pathMatch: "full" // URL extata (geralmente para home se passa FULL)
+  },
+  { 
+    path: "portifolio", // Com rotas filhas eu tenho duas formas de acessar a página com ou sem o id
+    component: CardComponent, // da forma abaixo (comentado) eu só posso acessar passando o id
+    children: [
+      {
+        path:":id",
+        component: CardComponent,
+      }
+    ]
+  },
+  /*{
+    path: "portifolio/:id", // rota com url/portifolio - o /:id cria uma variável chamada id cujo valor será passado após a barra
+    // e eu posso recuperá-lo posteriormente
+    component: CardComponent,
+    pathMatch: "prefix" // só o começo da url
+  },*/
+  {
+    path: '**', //qualquer coisa não mapeada,
+    redirectTo: "" // rota para o qual eu devo ser redirecionado caso eu acesse uma rota que não existe. No nosso caso vai para home
+    // pode ser usado para páginas de not found, por exemplo
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/portifolio/card/card.component.ts
+++ src/app/pages/portifolio/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private navegador: Router) {
+
+    //Para recuperar o id passado na url
+    //http://localhost:4200/portifolio/{id}
+      this.activeRoute.params.subscribe(
+         res => console.log(res)
+      ) //a função subscribe coloca os paramns no observable padrão do Amgular para que possamos
+      //usar esses elementos
+      // Dentro do ActivatedRoute nós conseguimos pegar os parâmetros e dentro do subscribe podemos trabalhar com eles
+
+      this.activeRoute.firstChild?.params.subscribe(
+        res => console.log(res) // para pegar os params de uma rota children
+     )
+
+      //para recuperar a Query param
+      //http://localhost:4200/portifolio/1?{name=Raul&surname=Silva}
+      this.activeRoute.queryParams.subscribe(
+          res => console.log(res)
+      )
+   }
 
   ngOnInit(): void {
+    /*setInterval(
+      () => {
+        this.navegador.navigate(['/'])
+      } , 5000
+    ) // Aqui o usuário é redirecionado para a home após 5 segundos.
+    */
   }
 
 }
